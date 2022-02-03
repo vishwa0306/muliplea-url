b673ae9e18c6bcf7c664b7e4e8759e276e27a9a2
vishwa0306 <96384704+vishwa0306@users.noreply.github.com>
2022-02-03 17:09:23 +0530

    dac
    sda

--- mutiple url/url.js
+++ mutiple url/url.js
@@ -1,5 +1,11 @@
-$('a.yourlink').click(function(e) {
-    e.preventDefault();
-    window.open('https://www.google.com');
-    window.open('https://www.google.com');
-});
+var myLinks = [
+"https://google.com",
+"https://www.w3schools.com/jsref/met_win_open.asp",
+"https://developer.mozilla.org/en-US/docs/Web/API/Window/open"
+]
+
+function openMultipleLinks(links) {
+  for (var i = 0; i < links.length; i ++) {
+    window.open(links[i]);
+  } 
+}
